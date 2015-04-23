20b94678ba9777706c7d92a45701c8606595c84c
kristapsu <steinbergs.kristaps@gmail.com>
2015-04-23 13:31:26 +0000

    23.04.2015
    Upper nav & header started

--- js/script.js
+++ js/script.js
@@ -1,6 +1,26 @@
-function resizeText(multiplier) {
-  if (document.body.style.fontSize == "5px") {
-    document.body.style.fontSize = "10px";
-  }
-  document.body.style.fontSize = parseFloat(document.body.style.fontSize) + (multiplier * 0.2) + "px";
+
+var imageArray = new Array(); // leave as is.
+
+// Specify number of milliseconds between image switches.
+var switchMilliseconds = 10000;
+
+// Specify the id of the div or other HTML tag with the 
+//   background image to switch.
+
+var divID = 'slideshow';
+
+// To add more images, continue the pattern below.
+
+imageArray[0] = 'img/header-background-1.png';
+imageArray[1] = 'img/header-background-2.png';
+imageArray[2] = 'img/header-background-3.png';
+
+// No further customization needed in this JavaScript
+
+function publishPicture(i) {
+document.getElementById(divID).style.background = 'url("'+imageArray[i]+'")';
+i++;
+if( i > (imageArray.length - 1) ) { i = 0; }
+setTimeout('publishPicture('+i+')',switchMilliseconds);
 }
+publishPicture(0);
